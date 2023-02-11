acc60799e72ae34e17690b4cf1260a6b155b870a
IT-Engineer-K <76894075+IT-Engineer-K@users.noreply.github.com>
2023-02-12 00:18:25 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var flag_speech = 0;
 const music = new Audio()
+const text_list = [];
 
 function vr_function() {
     window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
@@ -28,17 +29,26 @@ function vr_function() {
         var results = event.results;
         for (var i = event.resultIndex; i < results.length; i++) {
             if (results[i].isFinal) {
-                const request_text = results[i][0].transcript
-                document.getElementById('result_text').innerHTML = request_text;
+                const res_text = results[i][0].transcript
+                text_list.push(res_text)
+                if (text_list.length > 3)
+                    text_list.shift()
+                const request_text = text_list.join('\n')
+                console.log(request_text)
+                document.getElementById('result_text').innerHTML = res_text;
 
                 const formData = new FormData()
                 formData.append('text', request_text)
-                fetch('https://mudosheng-rishang-gele-tuan.onrender.com/', {
+                fetch('http://musicapi.pythonanywhere.com/' /*'http://127.0.0.1:5000' 'https://mudosheng-rishang-gele-tuan.onrender.com/'*/ , {
                     method: "POST",
                     mode: 'cors',
                     body: formData
                 }).then(res => res.text()).then(text => {
-                    console.log(`https://it-engineer-k.github.io/mood-moriage-gakudan/musics/${text}.mp3`)
+                    text = encodeURI(text)
+                    const newUrl = `https://it-engineer-k.github.io/mood-moriage-gakudan/musics/${text}.mp3`
+                    console.log(music.src, newUrl)
+                    if (music.src == newUrl)
+                        return
                     music.pause()
                     music.src = `https://it-engineer-k.github.io/mood-moriage-gakudan/musics/${text}.mp3`
                     music.play()
