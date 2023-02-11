366177ef846cdcd33d255ea85767d855ca0de636
IT-Engineer-K <76894075+IT-Engineer-K@users.noreply.github.com>
2023-02-12 00:44:41 +0900

    Update script.js

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ function vr_function() {
 
                 const formData = new FormData()
                 formData.append('text', request_text)
-                fetch('http://musicapi.pythonanywhere.com/' /*'http://127.0.0.1:5000' 'https://mudosheng-rishang-gele-tuan.onrender.com/'*/ , {
+                fetch('https://musicapi.pythonanywhere.com/' /*'http://127.0.0.1:5000' 'https://mudosheng-rishang-gele-tuan.onrender.com/'*/ , {
                     method: "POST",
                     mode: 'cors',
                     body: formData
